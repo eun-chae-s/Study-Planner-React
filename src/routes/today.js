ee624ad7bd05fa0f617c6ae580691214bd4fe460
eun-chae-s <s9710173@naver.com>
2022-01-14 01:00:38 -0500

    Finished most of design for the planner part

--- src/routes/today.js
+++ src/routes/today.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import './css/today.css';
-import Task from './task';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSmile, faSadCry, faMeh } from '@fortawesome/free-solid-svg-icons';
 
 class Today extends React.Component {
     constructor(props) {
         super(props);
+        // Connect to the database
         this.state = {
-            todayTodo: []
+            newTask: '',
+            todayTodo: [
+                {'task': 'MAT235 Lecture', 'completed': 'not yet'},
+                {'task': 'Make a React app', 'completed': 'yes'},
+                {'task': 'Workout', 'completed': 'no'},
+                {'task': 'Apply to internship', 'completed': 'next'},
+                {'task': 'Send an email', 'completed': 'not yet'}
+            ]
+        }
+
+        this.addNewTask = this.addNewTask.bind(this);
+        this.handleChangeNewTask = this.handleChangeNewTask.bind(this);
+    }
+
+    handleChangeNewTask(event) {
+        this.setState({newTask: event.target.value});
+    }
+
+    addNewTask() {
+        var rows = this.state.todayTodo;
+        rows.push({'task': this.state.newTask, 'completed': 'not yet'});
+        this.setState({todayTodo: rows, newTask: ''});
+    }
+
+    findRightEmoji(completed) {
+        if (completed === 'not yet') {
+            return <td></td>;
+        } else if (completed === 'no') {
+            return <td>&#10008;</td>;
+        } else if (completed === 'yes') {
+            return <td>&#10004;</td>;
+        } else {
+            return <td>&#10137;</td>;
         }
     }
+
     render() {
         return (
             <div>
@@ -36,26 +71,53 @@ class Today extends React.Component {
                         What's your plan for today?
                     </h1>
                     {/* new design for displaying date */}
-                    <h3>
-                        {new Date().toISOString().slice(0, 10)}
-                    </h3>
+                    <div className="today-date">
+                        <h3>
+                            {new Date().toISOString().slice(0, 10)}
+                        </h3>
+                    </div>
+                    
                     <div className="add">
-                        <input id="new-task" placeholder='Type new task'></input>
-                        <button id="add-new-task">
+                        <input id="new-task" placeholder='Type new task' value={this.state.newTask} onChange={this.handleChangeNewTask}></input>
+                        <button id="add-new-task" onClick={this.addNewTask}>
                             Add
                         </button>
                     </div>
-
                     <div className="todo-list">
-                        <ul>
-                            <li><Task task='hello' completion='finished'></Task></li>
-                        </ul>
+                        <table>
+                            <thead>
+                                <th>Task</th>
+                                <th>&#10003;</th>
+                            </thead>
+                            <tbody>
+                                {this.state.todayTodo.map((item) => 
+                                    <tr>
+                                        <td>{item.task}</td>
+                                        {this.findRightEmoji(item.completed)}
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
                     </div>
-                    <div className="timetable">
+                    <div className="rating">
                         <h3>
-                            Rate your day!
+                            How was your day?
                         </h3>
+                        {/* Need to make the icon clickable */}
+                        <FontAwesomeIcon icon={faSmile} size='4x' color='white'></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faSadCry} size='4x' color='white'></FontAwesomeIcon>
+                        <FontAwesomeIcon icon={faMeh} size='4x' color='yellow'></FontAwesomeIcon>
                     </div>
+                    
+                    <button id='save'>Save</button>
+                    {/* Spotify API */}
+                    {/* <div>
+                        <h3>
+                            Today's Song?
+                        </h3>
+                        <input id='song-search' placeholder='Type the title of the song'></input>
+                        <button id='search'>search</button>
+                    </div> */}
                 </div>
             </div>
         )
