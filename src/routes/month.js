e74cb7a7e1b1b07c3e9fc67e2d7121b827f37764
eun-chae-s <s9710173@naver.com>
2022-01-17 01:13:53 -0500

    working on the grid part

--- src/routes/month.js
+++ src/routes/month.js
@@ -5,6 +5,11 @@ import { Link } from 'react-router-dom';
 class Month extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            data: {1: ['soso', 70], 2: ['happy', 90], 
+            4: ['soso', 60], 8: ['sad', 40], 15:['happy', 80], 29:['soso', 70]
+            }
+        }
     }
 
     render() {
@@ -30,6 +35,22 @@ class Month extends React.Component {
                 </div> 
                 <div className="this-month-planners">
                     <h1>Here's your progress!</h1>
+                    <h2>{new Date().toISOString().slice(0, 7)}</h2>
+                    <div className="collection">
+                        {Array.from({length:31},(v,k)=>k+1).map((num) => {
+                            if (this.state.data[num] != null) {
+                                <div className={"item" + num}>
+                                    {this.state.data[num][0]}
+                                    {this.state.data[num][1]}
+                                </div>
+                            } else {
+                                <div className={"item" + num}>
+                                    hello
+                                </div>
+                            }
+                            
+                        })}
+                    </div>
                 </div>
             </div>
         )
