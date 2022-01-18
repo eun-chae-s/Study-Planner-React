e627bded85519b0d89b5298e2260cdcc1614d4cf
eun-chae-s <s9710173@naver.com>
2022-01-18 01:22:30 -0500

    Trying to use material ui

--- src/routes/month.js
+++ src/routes/month.js
@@ -1,6 +1,15 @@
 import './css/month.css';
 import React from 'react';
+import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
+import { Grid, Paper, Box } from '@mui/material';
+
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 class Month extends React.Component {
     constructor(props) {
@@ -36,21 +45,41 @@ class Month extends React.Component {
                 <div className="this-month-planners">
                     <h1>Here's your progress!</h1>
                     <h2>{new Date().toISOString().slice(0, 7)}</h2>
-                    <div className="collection">
+                    <Box sx={{ flexGrow: 1 }}>
+                        <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
+                            {Array.from({length:31},(v,k) => k+1).map((num) => {
+                                console.log(num);
+                                <Grid item xs={6} md={8}>
+                                    <Item>hello!</Item>
+                                </Grid>
+                                // if (this.state.data[num] != null) {
+                                //     <Grid item xs={6} md={8}>
+                                //         <Item>{this.state.data[num][0]}</Item>
+                                //     </Grid>
+                                // } else {
+                                    
+                                // }
+                                
+                            })}
+                        </Grid>
+                    </Box>
+                    
+                    {/* <div className="collection">
                         {Array.from({length:31},(v,k)=>k+1).map((num) => {
+                            console.log(num);
                             if (this.state.data[num] != null) {
-                                <div className={"item" + num}>
-                                    {this.state.data[num][0]}
-                                    {this.state.data[num][1]}
+                                <div className={"item"} id={"item" + num}>
+                                    <a>{this.state.data[num][0]}</a>
+                                    <a>{this.state.data[num][1]}</a>
                                 </div>
                             } else {
-                                <div className={"item" + num}>
+                                <div className={"item"} id={"item" + num}>
                                     hello
                                 </div>
                             }
                             
                         })}
-                    </div>
+                    </div> */}
                 </div>
             </div>
         )
